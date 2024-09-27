18c0c5c1ee5ab3dce94744b67a17657ff0b3ca9a
roger <paredes.roger@gmail.com>
2024-09-26 20:55:55 -0400

    2nd Commit, removed unused imports

--- animal_adoption_network/src/app/page.tsx
+++ animal_adoption_network/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
 import Header from "@/components/header";
 import Index from "@/components/index";
 import Footer from "@/components/footer";
