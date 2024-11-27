264281206d662d3ae40afa299c87206bdb851c0d
roger <rparedes@costra.ec>
2024-11-27 13:17:01 -0500

    Lovelists enabled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={` ${notoSans.variable} antialiased`}>
         {/* <Header /> */}
         {children}
